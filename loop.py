dd626e8122944a9ebe56da60405823f2ff131bac
Anshika Singh <114940394+anshikasdodwal@users.noreply.github.com>
2024-11-27 06:19:18 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,23 @@
+
+for data in 1.5,"Anshika",30:
+    print(data)
+
+for i in "Anshika":
+    print(i)
+print("---------------------------------------------------------------------------------------")
+"""range function :3 types
+          1. stop : for i in range(100): prints 0 to 99
+          2. start and stop: for i in range(10,20): prints 10 to 19
+          3. start, stop and step: for i in range(10,21,2): prints numbers with 2 ka gap
+"""
+for i in range(100):
+    print(i)
+
+print("--------------------------------------------------------------------------------------------")
+for i in range(10,21):
+    print(i)
+print("-------------------------------------------------------------------------------------------")
+for i in range(10,21,2):
+    print(i)
+
+
